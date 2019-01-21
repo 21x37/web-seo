5f590c9c8acceea5da429b67eecc61b3f6819ee2
21x37 <tylermok@outlook.com>
2019-01-21 15:38:01 -0800

    Added animations to services and testimonials

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+$('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+}, {
+    offset: '70%'
+});
+
+$('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+}, {
+    offset: '70%'
+});
